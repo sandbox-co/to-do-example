b9f86e4213d0bece85886bc4121467124c5dc4c3
Alexi Christakis <alexi.christakis@gmail.com>
2018-10-06 16:21:35 -0400

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [],
+    count: 0
+  };
+
+  addToDo = () => {
+    const todos = this.state.todos;
+    let id = this.state.count + 1;
+    let newToDo = {
+      id,
+      task: "I need to do this!"
+    };
+
+    this.setState({ todos: [...todos, newToDo], count: id });
+  };
+
+  removeToDo = id => {
+    const todos = this.state.todos;
+    this.setState({ todos: todos.filter(todo => todo.id !== id) });
+  };
+
+  toggleToDo = () => {};
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div onClick={this.addToDo}>add todo</div>
+        {this.state.todos.map(todo => (
+          <div onClick={() => this.toggleToDo(todo.id)}>
+            <h1>{todo.id}</h1>
+            <h2>{todo.task}</h2>
+            <div onClick={() => this.removeToDo(todo.id)}>remove</div>
+          </div>
+        ))}
       </div>
     );
   }
